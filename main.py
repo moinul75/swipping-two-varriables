38997b300f17b5281a231057870d3c4d19f59f6c
moinul75 <102654562+moinul75@users.noreply.github.com>
2022-04-04 13:15:02 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+a = 10
+b = 20
+
+a, b = b,a
+print("a = ", a)
+print("b = ", b)
+
+#solve using temp 
+
+a = 10
+b = 20
+
+temp = a
+a = b
+temp = b
+
+print("the value of a after swipping: {}" , format(x))
+print("the value of b after swipping: {}", format(y))
